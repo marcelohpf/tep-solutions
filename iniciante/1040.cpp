226e5c824e30d3007e940cc1cab639f338bdecef
Mateus Furquim <furquim.axs@gmail.com>
2016-03-23 15:34:20 -0300

    Questão 1040 resolvida.

--- iniciante/1040.cpp
+++ iniciante/1040.cpp
@@ -1,33 +1,38 @@
 #include <bits/stdc++.h>
 #include <array>
 
-#define PRECISION 2
+#define PRECISION 1
 
 using namespace std;
-
-array<pair<int, double>, 5> itens;
 
 int main()
 {
     cout << fixed << setprecision(PRECISION);
 
-    itens[0] = make_pair(1, 4.0);
-    itens[1] = make_pair(2, 4.5);
-    itens[2] = make_pair(3, 5.0);
-    itens[3] = make_pair(4, 2.0);
-    itens[4] = make_pair(5, 1.5);
+    double a, b, c, d; cin >> a >> b >> c >> d;
+    double media = (2*a + 3*b + 4*c + 1*d)/10;
 
-    int a, b; cin >> a >> b;
-    double ammount = 0;
-
-    for (size_t i = 0; i < 5; i++) {
-        if (a == itens[i].first) {
-            ammount = itens[i].second;
-            break;
-        }
+    cout << "Media: " << media << endl;
+    if (media >= 7) {
+        cout << "Aluno aprovado." << endl;
     }
+    else if (media >= 5) {
+        cout << "Aluno em exame." << endl;
+        double exame; cin >> exame;
+        cout << "Nota do exame: " << exame << endl;
 
-    cout << "Total: R$ " << ammount*b << endl;
+        if ((media+exame)/2 < 5) {
+            cout << "Aluno reprovado." << endl;
+            cout << "Media final: " << (media+exame)/2 << endl;
+        }
+        else {
+            cout << "Aluno aprovado." << endl;
+            cout << "Media final: " << (media+exame)/2 << endl;
+        }
+    } else {
+        cout << "Aluno reprovado." << endl;
+    }
+//    cerr << "a: " << a << "\tb: " << b << "\tc: " << c << "\td: " << d << "\nmedia: " << media << endl;
 
     return 0;
 }
